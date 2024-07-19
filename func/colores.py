efd18cbb2a34d5f55a2c1489ed3c9868b8ddfc3a
Alejandro Tolaba <alejandrotolaba@gmail.com>
2024-07-18 23:54:10 -0300

    Nuevas paletas de colore (como comentarios por ahora)

--- func/colores.py
+++ func/colores.py
@@ -1,14 +1,51 @@
 import pyray as pr
 
 
+# PALETA DEL PROGRAMADOR
+# COLORES = [
+# 		pr.Color(32, 32, 32, 255),
+# 		pr.Color(255, 128, 64, 255),
+# 		pr.Color(255, 64, 128, 255),
+# 		pr.Color(128, 255, 64, 255),
+# 		pr.Color(128, 64, 255, 255),
+# 		pr.Color(64, 255, 128, 255),
+# 		pr.Color(64, 128, 255, 255),
+# 		pr.Color(255, 255, 64, 255),
+# 	]
 
+# GLORIA'S FIRST PALETTE
+# COLORES = [
+# 		pr.Color(0, 1, 49, 255),
+# 		pr.Color(16, 202, 163, 255),
+# 		pr.Color(106, 20, 205, 255),
+# 		pr.Color(156, 0, 206, 255),
+# 		pr.Color(205, 20, 114, 255),
+# 		pr.Color(0, 154, 206, 255),
+# 		pr.Color(41, 202, 0, 255),
+# 		pr.Color(16, 201, 163, 255),
+# 	]
+
+# # TERRACOTA
+# COLORES = [
+# 		pr.Color(16, 0, 26, 255),
+# 		pr.Color(188, 109, 156, 255),
+# 		pr.Color(231, 113, 148, 255),
+# 		pr.Color(222, 77, 82, 255),
+# 		pr.Color(207, 20, 99, 255),
+# 		pr.Color(213, 62, 148, 255),
+# 		pr.Color(222, 122, 73, 255),
+# 		pr.Color(221, 121, 73, 255),
+# 	]
+
+# Vanellope
 COLORES = [
-		pr.Color(32, 32, 32, 255),
-		pr.Color(255, 128, 64, 255),
-		pr.Color(255, 64, 128, 255),
-		pr.Color(128, 255, 64, 255),
-		pr.Color(128, 64, 255, 255),
-		pr.Color(64, 255, 128, 255),
-		pr.Color(64, 128, 255, 255),
-		pr.Color(64, 255, 64, 255),
+		pr.Color(25, 0, 32, 255),
+		pr.Color(214, 97, 149, 255),
+		pr.Color(99, 209, 157, 255),
+		pr.Color(99, 174, 214, 255),
+		pr.Color(214, 97, 149, 255),
+		pr.Color(98, 210, 155, 255),
+		pr.Color(213, 178, 98, 255),
+		pr.Color(141, 117, 214, 255),
 	]
+
